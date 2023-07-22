050d60635570bb42a2a36ed27b80fb9a0f833015
“airman416” <im.armaan.agrawal@gmail.com>
2023-07-22 18:48:17 +0200

    Simplified view, moved upload button

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,7 @@ fetch('https://api.cloudinary.com/v1_1/dwkri0lj3/resources/image', { headers: he
         console.log(data);
     });
 
-for (let i = 0; i < 50; i++) {
+for (let i = 0; i < 10; i++) {
     document.getElementById('gallery').innerHTML += `
         <li><img
         src="https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638882786/EducationHub/photos/sun-blasts-a-m66-flare.jpg">
@@ -24,17 +24,17 @@ for (let i = 0; i < 50; i++) {
     `;
 }
 
-let scrollHeight = 0;
-document.addEventListener('scroll', () => {
-    if (document.documentElement.scrollTop > document.documentElement.scrollHeight - 2000) {
-        console.log("true");
-        document.getElementById('upload_widget').style.display = 'none';
-    } else {
-        console.log("false");
-        document.getElementById('upload_widget').style.display = 'block';
-    }
-}
-);
+// let scrollHeight = 0;
+// document.addEventListener('scroll', () => {
+//     if (document.documentElement.scrollTop > document.documentElement.scrollHeight - 2000) {
+//         console.log("true");
+//         document.getElementById('upload_widget').style.display = 'none';
+//     } else {
+//         console.log("false");
+//         document.getElementById('upload_widget').style.display = 'block';
+//     }
+// }
+// );
 
 // get current scroll position
 
